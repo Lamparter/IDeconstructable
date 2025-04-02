bf86100fbcc03706a28275c3a5a447f765b01c51
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-04-02 17:56:40 +0100

    Add project files

---
+++ Riverside.DeconstructableInterfaces/IDeconstructable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Riverside.DeconstructableInterfaces;
+
+/// <summary>
+/// An interface for types that can be deconstructed into two non-nullable components.
+/// </summary>
+/// <typeparam name="T1">Represents the first component of the deconstructed value.</typeparam>
+/// <typeparam name="T2">Represents the second component of the deconstructed value.</typeparam>
+public interface IDeconstructable<T1, T2>
+{
+    (T1, T2) Deconstruct();
+}
